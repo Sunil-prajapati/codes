f1762a48eec470f1099a9fc3d561e78e92a8ddab
Sunil Prajapati <sunil.prajapati@statrys.com>
2024-08-10 20:08:48 +0530

    resolved some question

--- programming/string.js
+++ programming/string.js
@@ -38,3 +38,49 @@ function missingNum(arr) {
   return number;
 }
 missingNum([10, 5, 1, 2, 4, 6, 8, 3, 9]);
+
+// The Fizz Buzz Test
+function fizzBuzz(num) {
+  const result = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+fizzBuzz(15);
+
+// Tower of Hanoi
+function towerHanoi(num) {
+  return Math.pow(2, num) - 1;
+}
+towerHanoi(5);
+
+// A Simple Check
+function findSmallerNumber(a, b) {
+  return a < b ? a : b;
+}
+
+function simpleCheck(a, b) {
+  let divisibleCount = 0;
+  const smaller = findSmallerNumber(a, b);
+  while (smaller > 0) {
+    if (b % a === 0) {
+      divisibleCount++;
+    }
+    a--;
+    b--;
+  }
+
+  return divisibleCount;
+}
+
+const result = simpleCheck(2, 4);
+console.log(result);
